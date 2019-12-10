05b9200029c17425b25af263bd373cdaee4c33dd
Mark Zhigulin <forsteam.markoni@mail.ru>
2019-12-10 22:24:38 +0300

    fisrt steps

--- Board/views.py
+++ Board/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def tasks(request):
+    return render(request, 'tasks.html', {'tasks':Task.objects.all()})
