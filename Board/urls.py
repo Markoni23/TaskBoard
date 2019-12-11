72448458d0cb22fa984fa8237e73cf1ef134f64d
Mark Zhigulin <forsteam.markoni@mail.ru>
2019-12-11 15:23:12 +0300

    begin detailing tasks

--- Board/urls.py
+++ Board/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('tasks/', tasks),
+    path('tasks/', TaskListView.as_view()),
+    path('task/<int:pk>', TaskDetailView.as_view()),
     path('hi/', hi),
 ]
--- Board/views.py
+++ Board/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView, DetailView
 from .models import *
 
 # Create your views here.
@@ -7,5 +9,15 @@ def tasks(request):
     return render(request, 'tasks.html', {'tasks':Task.objects.all()})
 
 
+class TaskListView(ListView):
+    queryset = Task.objects.all()
+    context_object_name = 'tasks'
+    template_name = 'Board/tasks.html'
+    
+
+class TaskDetailView(DetailView):
+    model = Task
+
+
 def hi(request):
-    return render(request, 'main.html')
+    return render(request, 'Board/main.html')
